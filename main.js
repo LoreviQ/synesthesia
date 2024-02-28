5400452c93803ee526b6c7c44b5bcea6fb1f175f
LoreviQ <lorevi@protonmail.com>
2024-02-28 19:29:43 +0000

    Updated code to convert any word into a search query

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const { search } = require("./search.js");
 
 async function main() {
-    searchQuery =
-        "https://www.google.com/search?sca_esv=2802dad21c2aa82f&rlz=1C1GCEA_enGB1088GB1088&sxsrf=ACQVn0-8Mn5WhvzYZuxhAdxeT5ObpwMDgQ:1709147063177&q=lucina&tbm=isch&source=lnms&sa=X&ved=2ahUKEwib2cb83M6EAxVy_bsIHfYDAQEQ0pQJegQIBhAB&biw=2048&bih=1066&dpr=1.25";
-    search(searchQuery);
+    const word = "test";
+    convertWordToColour(word);
 }
 
 main();
--- search.js
+++ search.js
@@ -1,6 +1,21 @@
-async function search(searchQuery) {
-    const response = await fetch(searchQuery);
-    console.log(await response.text());
+const url = require("node:url");
+const { JSDOM } = require("jsdom");
+
+async function convertWordToColour(word) {
+    const htmlBody = search(word);
 }
 
-module.exports = { search };
+async function search(searchQuery) {
+    const searchURL =
+        "https://www.google.com/search?&tbm=isch&q=" + searchQuery;
+    const response = await fetch(searchURL);
+    if (response.status >= 400) {
+        throw Error(`HTTP Error Code: ${response.status}`);
+    }
+    if (!response.headers.get("Content-Type").includes("text/html")) {
+        throw Error(`Content type is not 'text/html'`);
+    }
+    return await response.text();
+}
+
+module.exports = { convertWordToColour };
